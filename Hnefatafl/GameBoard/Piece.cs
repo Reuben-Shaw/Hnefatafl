b4fe2b9c99c6b0d3d57d51622dc403118e1ec410
PleccyMM <pleccymm@outlook.com>
2022-02-16 14:34:13 +0000

    Initial 2

--- Hnefatafl/GameBoard/Piece.cs
+++ Hnefatafl/GameBoard/Piece.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Content;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using Hnefatafl;
 using static Hnefatafl.PieceType;
